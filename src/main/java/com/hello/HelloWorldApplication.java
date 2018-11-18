5771d0216226acb4b439c56432ba25332b5e5779
Pete <peter.solomons@hotmail.co.uk>
2018-11-18 13:48:11 +0000

    removing spring security login for the app

--- src/main/java/com/hello/HelloWorldApplication.java
+++ src/main/java/com/hello/HelloWorldApplication.java
@@ -2,8 +2,9 @@ package com.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class HelloWorldApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
